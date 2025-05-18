4088f6409d495807edefd67220b5aa5f00e66265
Emilio Ochoa Alva <eochoaal@ur.rochester.edu>
2025-05-18 00:02:56 -0400

    Update assgan.sh

--- assgan.sh
+++ assgan.sh
@@ -3,7 +3,7 @@
 #SBATCH -p reserved --reservation=bcastane_12222024
 #SBATCH --time=01:00:00
 #SBATCH --gres=gpu:2
-#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-node=2s
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=16G
 #SBATCH --output=test_reserved2.log
