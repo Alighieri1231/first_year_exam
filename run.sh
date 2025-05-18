9b5c104a2962a19baec038572db37a13c0e80d6c
Emilio Ochoa Alva <eochoaal@ur.rochester.edu>
2025-05-17 20:08:43 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,8 +12,8 @@ for rep in "${REPS[@]}"; do
   for DS in data_uncorrected data_corrected_w_annotation data_corrected_wo_annotation; do
     for arch in "${ARCHS[@]}"; do
       TRAIN_CSV="${BASE_DATA}/${DS}/train.csv"
-      DEV_CSV  ="${BASE_DATA}/${DS}/validation.csv"
-      TEST_CSV ="${BASE_DATA}/${DS}/test.csv"
+      DEV_CSV="${BASE_DATA}/${DS}/validation.csv"
+      TEST_CSV="${BASE_DATA}/${DS}/test.csv"
       RUNID="${DS}_r${rep}_${arch}"
       echo "=== Run: ${RUNID} ==="
       $CMD \
@@ -35,8 +35,8 @@ for rep in "${REPS[@]}"; do
   for suf in "${SUFFIXES[@]}"; do
     for arch in "${ARCHS[@]}"; do
       TRAIN_CSV="${BASE_DATA}/train_${suf}.csv"
-      DEV_CSV  ="${BASE_DATA}/validation_${suf}.csv"
-      TEST_CSV ="${BASE_DATA}/test_${suf}.csv"
+      DEV_CSV="${BASE_DATA}/validation_${suf}.csv"
+      TEST_CSV="${BASE_DATA}/test_${suf}.csv"
       RUNID="uncorrected_${suf}_r${rep}_${arch}"
       echo "=== Run: ${RUNID} ==="
       $CMD \
