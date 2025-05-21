5d19d1351914bcc0533e5a2cf6aeaed3d94b481d
Emilio Ochoa Alva <eochoaal@ur.rochester.edu>
2025-05-21 15:35:08 -0400

    .

--- run.sh
+++ run.sh
@@ -9,31 +9,31 @@ CMD="python -m src.train_lightning_seg"
 
 #1) Carpetas × réplicas × archs
 
-for rep in "${REPS[@]}"; do
-  for DS in data_uncorrected data_corrected_w_annotation data_corrected_wo_annotation; do
-    for arch in "${ARCHS[@]}"; do
-      TRAIN_CSV="${BASE_DATA}/${DS}/train.csv"
-      DEV_CSV="${BASE_DATA}/${DS}/validation.csv"
-      TEST_CSV="${BASE_DATA}/${DS}/test.csv"
-      RUNID="${DS}_r${rep}_${arch}"
-      echo "=== Run: ${RUNID} ==="
-      $CMD \
-        -c "${BASE_CFG}" \
-        --run-id "${RUNID}" \
-        --arch-override "${arch}" \
-        --train-file-override "${TRAIN_CSV}" \
-        --dev-file-override   "${DEV_CSV}" \
-        --test-file-override  "${TEST_CSV}"
-    done
-  done
-done
+# for rep in "${REPS[@]}"; do
+#   for DS in data_uncorrected data_corrected_w_annotation data_corrected_wo_annotation; do
+#     for arch in "${ARCHS[@]}"; do
+#       TRAIN_CSV="${BASE_DATA}/${DS}/train.csv"
+#       DEV_CSV="${BASE_DATA}/${DS}/validation.csv"
+#       TEST_CSV="${BASE_DATA}/${DS}/test.csv"
+#       RUNID="${DS}_r${rep}_${arch}"
+#       echo "=== Run: ${RUNID} ==="
+#       $CMD \
+#         -c "${BASE_CFG}" \
+#         --run-id "${RUNID}" \
+#         --arch-override "${arch}" \
+#         --train-file-override "${TRAIN_CSV}" \
+#         --dev-file-override   "${DEV_CSV}" \
+#         --test-file-override  "${TEST_CSV}"
+#     done
+#   done
+# done
 
 #
 # 2) Sufijos dentro de data_uncorrected × réplicas × archs
-#
+##
 SUFFIXES=(l nl lb lm)
 #SUFFIXES=(l nl)
-#SUFFIXES=(l lm)
+SUFFIXES=(l lm)
 
 for rep in "${REPS[@]}"; do
   for suf in "${SUFFIXES[@]}"; do
